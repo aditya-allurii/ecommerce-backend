fab28fe44fa27592a3f0dcf6a433808f43bf7b58
Aditya <apple@Adityas-MacBook-Air.local>
2026-06-09 12:55:22 +0530

    fixed bugs

--- src/main/java/com/ecommerce/project/security/WebConfig.java
+++ src/main/java/com/ecommerce/project/security/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "https://shopvyra.vercel.app")
+                .allowedOriginPatterns("http://localhost:*", frontEndUrl)
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
