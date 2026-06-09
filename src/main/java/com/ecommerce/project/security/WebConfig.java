1c49b6ae6887bf14aa4a2ded6fed1cd23cf9ee15
Aditya <apple@Adityas-MacBook-Air.local>
2026-06-09 11:29:36 +0530

    fixed bugs

--- src/main/java/com/ecommerce/project/security/WebConfig.java
+++ src/main/java/com/ecommerce/project/security/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:*", frontEndUrl)
+                .allowedOriginPatterns("http://localhost:*", frontEndUrl)
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
